ea737e45562a785cb55852d4e2e9f94766661bbb
Abdulmelik Ersoy <kellemen23@gmail.com>
2022-01-15 12:37:24 -0500

    ADDED PROCESS.ENV

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./routes/apiRoute');
 const htmlRoutes = require('./routes/htmlRoute');
 
 const app = express();
-const PORT = 8001;
+const PORT = process.env.PORT || 8001; 
 
 // MIDDLEWARE
 app.use(express.urlencoded({ extended: true}));
